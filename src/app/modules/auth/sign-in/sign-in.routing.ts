338354d9a99bedeb8af40b34ef76a26fc1dca771
Adriano Rabello <adrianor.rabello@hotmail.com>
2022-05-19 16:38:15 -0300

    First commit

---
+++ src/app/modules/auth/sign-in/sign-in.routing.ts
@@ -0,0 +1,13 @@
+import { Route } from '@angular/router';
+import { AuthSignInComponent } from 'app/modules/auth/sign-in/sign-in.component';
+
+export const authSignInRoutes: Route[] = [
+    {
+        path     : '',
+        component: AuthSignInComponent
+    },
+    {
+        path     : 'adriano-rabello',
+        component: AuthSignInComponent,
+    }
+];
